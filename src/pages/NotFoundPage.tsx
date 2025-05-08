17e82222afbd3c0d636a314c1858919e2f282221
Techie Pugal <pugalpugalee333@gmail.com>
2025-05-08 18:00:17 +0530

    map url update

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -150,7 +150,7 @@ const ContactPage: React.FC = () => {
           
           <div className="rounded-lg overflow-hidden shadow-lg">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.2291245042566!2d76.99013931474925!3d11.017897892155344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba859af2f971cb5%3A0x2fc1c81e4dda8648!2sCoimbatore%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1645548145782!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31326.079044839957!2d76.982782!3d11.056621!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba85875f5da5219%3A0xf0d8694e6c43d621!2sSUPER%20MACHINE%20WORKS%20(P)%20LTD!5e0!3m2!1sen!2sin!4v1714193586373!5m2!1sen!2sin"
               width="100%"
               height="450"
               style={{ border: 0 }}
