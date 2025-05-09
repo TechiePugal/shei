c487fdacf1ebc540ec99222067205ceb3be1db49
Techie Pugal <pugalpugalee333@gmail.com>
2025-05-09 14:34:44 +0530

    main update

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,15 @@
-import { StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
-import React from 'react';
-
-
-// Preloading the fonts for faster rendering
-const PreloadFonts = () => (
-  <>
-    <link 
-      rel="preload" 
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" 
-      as="style" 
-    />
-    <link 
-      rel="stylesheet" 
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" 
-    />
-  </>
-);
-
-const LazyApp = React.lazy(() => import('./App'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <PreloadFonts />
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <LazyApp />
-        </Suspense>
+        <App />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
