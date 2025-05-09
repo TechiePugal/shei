ab6268b6da04e3b6b032190f26a0cc8867387c2d
Techie Pugal <pugalpugalee333@gmail.com>
2025-05-09 12:44:14 +0530

    undo vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],  // Excluding lucide-react from dependency optimization
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split large libraries into separate chunks to reduce initial load size
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react';
-            if (id.includes('framer-motion')) return 'vendor-motion';
-            return 'vendor'; // Default chunk for other node_modules dependencies
-          }
-        },
-      },
-    },
+    exclude: ['lucide-react'],
   },
 });
